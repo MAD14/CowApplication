1626c650dc564d09c488fab5ba99ebdfb5e883df
elena93625 <elena93@gmail.com>
2017-04-05 12:19:42 +0200

    Versione con tutte le funzionalità per il second assignment
    Rimane da risolvere il bug.
    Bisogna aggiustare gli aspett

--- app/src/main/java/it/polito/cowapplication/GroupActivity.java
+++ app/src/main/java/it/polito/cowapplication/GroupActivity.java
@@ -1,21 +1,14 @@
 package it.polito.cowapplication;
 
-import android.content.Intent;
-import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -28,6 +21,11 @@ public class GroupActivity extends AppCompatActivity {
     private List<ExpenseItem> expenses;
     private List<DebitsItem> debits;
 
+    // these arrays of strings are used to populate the List "expenses" and "debits" that are then shown in the app
+    // they will be then substituted by the data coming from the database
+    private String[] expenses_base = {"Expense1", "Expense2", "Expense3", "Expense4", "Expense5"};
+    private String[] value_expenses_base = {"13.50€", "1.50€", "21.00€", "8.30€", "15.00€"};
+    private String[] names_base = {"Michela", "Matteo", "Simone", "Greta", "Agnese"};
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,15 +36,12 @@ public class GroupActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     list = (ListView) findViewById(R.id.lv_expenses);
                     BaseAdapter bae = getAdapterExpenses();
-                    //bae.registerDataSetObserver(getObserverExpenses());
                     list.setAdapter(bae);
                     break;
 
                 case R.id.navigation_dashboard:
                     list = (ListView) findViewById(R.id.lv_expenses);
                     BaseAdapter bad = getAdapterDebit();
-                    //DataSetObserver observer1 = new DataSetObserver() {};
-                    //bad.registerDataSetObserver(observer1);
                     list.setAdapter(bad);
                     break;
             }
@@ -62,12 +57,8 @@ public class GroupActivity extends AppCompatActivity {
         expenses = new ArrayList<>();
         debits = new ArrayList<>();
 
-        for (int i=0;i<5;i++)
-            debits.add(new DebitsItem("Person " + i, String.valueOf(2.1*i)));
-
         list = (ListView) findViewById(R.id.lv_expenses);
         BaseAdapter bae = getAdapterExpenses();
-        //bae.registerDataSetObserver(getObserverExpenses());
         list.setAdapter(bae);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -77,8 +68,8 @@ public class GroupActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ExpenseItem exp = new ExpenseItem("Expense "+expenses.size(),String.valueOf(2.3*expenses.size()));
-                expenses.add(exp);
+                expenses.add(new ExpenseItem(expenses_base[expenses.size()], value_expenses_base[expenses.size()]));
+                debits.add(new DebitsItem(names_base[debits.size()], value_expenses_base[debits.size()]));
                 ((BaseAdapter) list.getAdapter()).notifyDataSetChanged();
             }
         });
@@ -117,119 +108,37 @@ public class GroupActivity extends AppCompatActivity {
         };
         return bae;
     }
-        //list.setAdapter(new ArrayAdapter<String>(this, R.layout.expense_item,expensesList));
-         /*ListAdapter lae = new ListAdapter() {
+
+    protected BaseAdapter getAdapterDebit() {
+        BaseAdapter bad = new BaseAdapter() {
             @Override
-            public boolean areAllItemsEnabled() {return true;}
+            public int getCount() {
+                return debits.size();
+            }
+
             @Override
-            public boolean isEnabled(int position) {return true;}
+            public Object getItem(int position) {
+                return debits.get(position);
+            }
+
             @Override
-            public void registerDataSetObserver(DataSetObserver observer) {  }
-            @Override
-            public void unregisterDataSetObserver(DataSetObserver observer) { }
-            @Override
-            public int getCount() {return expenses.size(); }
-            @Override
-            public Object getItem(int position) {return expenses.get(position); }
-            @Override
-            public long getItemId(int position) {return 0;}
-            @Override
-            public boolean hasStableIds() {return false;}
+            public long getItemId(int position) {
+                return 0;
+            }
+
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 if (convertView == null)
-                    convertView = getLayoutInflater().inflate(R.layout.expense_item, parent, false);
-                TextView tv = (TextView) convertView.findViewById(R.id.tv_expense);
-                tv.setText(expenses.get(position).getName());
+                    convertView = getLayoutInflater().inflate(R.layout.personal_summary_item, parent, false);
+                TextView tv = (TextView) convertView.findViewById(R.id.personal_column1);
+                tv.setText(debits.get(position).getName());
                 tv = (TextView) convertView.findViewById(R.id.personal_column2);
-                tv.setText(expenses.get(position).getValue());
+                tv.setText(debits.get(position).getValue());
                 FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-                fab.show();
+                fab.hide();
                 return convertView;
             }
-            @Override
-            public int getItemViewType(int position) {return 0;}
-            @Override
-            public int getViewTypeCount() {return 0;}
-            @Override
-            public boolean isEmpty() {
-                if (expenses.size()==0){
-                    return true;} else{
-                    return false;}
-            }
-        };*/
-
-protected BaseAdapter getAdapterDebit() {
-    BaseAdapter bad = new BaseAdapter() {
-        @Override
-        public int getCount() {
-            return debits.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return debits.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null)
-                convertView = getLayoutInflater().inflate(R.layout.personal_summary_item, parent, false);
-            TextView tv = (TextView) convertView.findViewById(R.id.personal_column1);
-            tv.setText(debits.get(position).getName());
-            tv = (TextView) convertView.findViewById(R.id.personal_column2);
-            tv.setText(debits.get(position).getValue());
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.hide();
-            return convertView;
-        }
-    };
-    return bad;
-}
-    /*ListAdapter lad = new ListAdapter() {
-        @Override
-        public boolean areAllItemsEnabled() {return true; }
-        @Override
-        public boolean isEnabled(int position) {return true;}
-        @Override
-        public void registerDataSetObserver(DataSetObserver observer) { }
-        @Override
-        public void unregisterDataSetObserver(DataSetObserver observer) {  }
-        @Override
-        public int getCount() {return debits.size();}
-        @Override
-        public DebitsItem getItem(int position) { return debits.get(position);}
-        @Override
-        public long getItemId(int position) {return 0;}
-        @Override
-        public boolean hasStableIds() {return false;}
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null)
-                convertView = getLayoutInflater().inflate(R.layout.personal_summary_item, parent, false);
-            TextView tv = (TextView) convertView.findViewById(R.id.personal_column1);
-            tv.setText(debits.get(position).getName());
-            tv = (TextView) convertView.findViewById(R.id.personal_column2);
-            tv.setText(debits.get(position).getValue());
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            fab.hide();
-            return convertView;
-        }
-        @Override
-        public int getItemViewType(int position) {return 0;}
-        @Override
-        public int getViewTypeCount() {return 0;}
-        @Override
-        public boolean isEmpty() {
-            if (debits.size()==0)
-                return true;
-            return false;
-        }
-    };*/
-
+        };
+        return bad;
+    }
 }
